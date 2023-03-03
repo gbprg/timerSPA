b85720f2b558cd7aa1aa69627337182560c9d0d2
gbprg <gabrielgbd22@hotmail.com>
2023-03-03 14:24:34 -0300

    finished-project

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,17 +1,11 @@
-import { createContext, ReactNode, useState } from "react";
+import { differenceInSeconds } from "date-fns/esm";
+import { createContext, ReactNode, useState, useReducer, useEffect } from "react";
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer"
 
 interface CreateCycleData {
   task: string
   minutesAmount: number
-}
-
-interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptedDate?: Date
-  finishedDate?: Date
 }
 
 interface CyclesContextType {
@@ -34,26 +28,48 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({ 
   children, 
 }: CyclesContextProviderProps) {
-  const [cycles, setCycles] = useState<Cycle[]>([]) 
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const [cyclesState, dispatch] = useReducer(cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    }, 
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        "@timer-do-gbd:cycles-state",
+      )
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+      return initialState
+    },
+  )
 
+  const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(
+        new Date(),
+        new Date(activeCycle.startDate)
+      )
+    }
+    
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem("@timer-do-gbd:cycles-state", stateJSON)
+  }, [cyclesState])
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds) 
   }
 
   function markCurrentCycleAsFinished() {
-    setCycles((state) =>
-      cycles.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, finishedDate: new Date()}
-        } else {
-          return cycle
-        }
-      }), 
-    )
+    dispatch(markCurrentCycleAsFinishedAction)
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -63,23 +79,15 @@ export function CyclesContextProvider({
       minutesAmount: data.minutesAmount,
       startDate: new Date(),
     }
+
+    dispatch(addNewCycleAction(newCycle))
     //sempre que uma alteração de estado depender do valor anterior, usa-se o formato de => //
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleId(newCycle.id)
+
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    setCycles((state) =>
-      cycles.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date()}
-        } else {
-          return cycle
-        }
-      }), 
-    )
-    setActiveCycleId(null)
+    dispatch(interruptCurrentCycleAction)
   }
   return (
     <CyclesContext.Provider
